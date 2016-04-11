a156ed3bd44aefa7dfafabb9cd8598e981e1261b
Karl Hobley <karlhobley10@gmail.com>
2016-04-11 23:20:30 +0100

    Use non-bc static assets

--- wagtailcondensedinlinepanel/wagtail_hooks.py
+++ wagtailcondensedinlinepanel/wagtail_hooks.py
@@ -7,9 +7,9 @@ from wagtail.wagtailcore import hooks
 
 @hooks.register('insert_editor_js')
 def editor_js():
-    return '<script src="{0}"></script>'.format(static('wagtailcondensedinlinepanel/compiled/js/condensedinlinepanel_bc.js'))
+    return '<script src="{0}"></script>'.format(static('wagtailcondensedinlinepanel/compiled/js/condensedinlinepanel.js'))
 
 
 @hooks.register('insert_editor_css')
 def editor_css():
-    return '<link rel="stylesheet" href="{0}">'.format(static('wagtailcondensedinlinepanel/compiled/css/condensedinlinepanel_bc.css'))
+    return '<link rel="stylesheet" href="{0}">'.format(static('wagtailcondensedinlinepanel/compiled/css/condensedinlinepanel.css'))
