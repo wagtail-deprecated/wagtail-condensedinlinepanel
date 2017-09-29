955b62e2befb4d098efba955b82afd2eeee01c2e
Karl Hobley <karl@kaed.uk>
2017-09-29 15:23:44 +0100

    Update tooling

--- webpack.config.js
+++ webpack.config.js
@@ -12,18 +12,17 @@ module.exports = {
 
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+        extensions: [".ts", ".tsx", ".js"]
     },
 
     module: {
-        loaders: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
-            { test: /\.tsx?$/, loader: "ts-loader" }
-        ],
-
-        preLoaders: [
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { test: /\.js$/, loader: "source-map-loader" }
+        rules: [
+            {
+                enforce: "pre",
+                test: /\.tsx?$/,
+                exclude: ["node_modules"],
+                use: ["ts-loader", "source-map-loader"]
+            },
         ]
     },
 };
